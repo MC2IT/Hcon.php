66e8a4caaba046728b3a988f4e89ce1fec44dbe3
Cédric Belin <cedx@outlook.com>
2026-09-05 21:55:52 +0200

    Add the "Invoke" script

---
+++ Invoke.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/env pwsh
+param (
+	# The name of the task to invoke.
+	[Parameter(Position = 1)]
+	[ArgumentCompleter({
+		param ([string] $commandName, [string] $parameterName, [string] $wordToComplete)
+		(Get-Item "$PSScriptRoot/Scripts/$wordToComplete*.ps1").BaseName
+	})]
+	[ValidateScript({ Test-Path "$PSScriptRoot/Scripts/$_.ps1" -PathType Leaf }, ErrorMessage = "The specified command does not exist.")]
+	[string] $Command = "Default"
+)
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+& "$PSScriptRoot/Scripts/$Command.ps1"
