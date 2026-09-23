d9f77b11841b52ac820f227e67858ca591e102fa
Cédric Belin <cedx@outlook.com>
2026-09-23 17:55:05 +0200

    Update the dependencies [skip ci]

--- Scripts/Update.ps1
+++ Scripts/Update.ps1
@@ -2,5 +2,5 @@ using module ./Cmdlets.psm1
 
 "Updating the dependencies..."
 $modules = Import-PowerShellDataFile PSModules.psd1
-foreach ($key in $modules.Keys) { Update-PSResource $key -Repository $modules[$key].repository -TrustRepository }
+$modules.Keys | ForEach-Object { Update-PSResource $_ -Repository $modules[$_].repository -TrustRepository }
 Update-ComposerPackage
